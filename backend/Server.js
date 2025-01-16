0b678e17b3595e2480f876894bd39844783a6664
Naresh <banvro07@gmail.com>
2025-01-16 17:25:02 +0530

    cahnge mooes

--- backend/Server.js
+++ backend/Server.js
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-
+// /todoforserver
 mongoose
-  .connect("mongodb://127.0.0.1:27017/todoforserver")
+  .connect("mongodb://127.0.0.1:27017")
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
