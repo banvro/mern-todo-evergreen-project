638f830d2c1cd2c8914558b53e9e0bb47b733c3f
Naresh <banvro07@gmail.com>
2025-01-16 16:00:59 +0530

    ever green todo

---
+++ backend/Server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const routes = require("./routes/ToDoRoutes");
+
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/todoforserver")
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+app.use("/api", routes);
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}...`));
