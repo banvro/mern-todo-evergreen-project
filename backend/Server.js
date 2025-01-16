37c3e93265f082b4c755755b5fe6188952134da4
Naresh <banvro07@gmail.com>
2025-01-16 17:53:12 +0530

    mogies

--- backend/Server.js
+++ backend/Server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const routes = require("./routes/ToDoRoutes");
-
 const cors = require("cors");
 
 const app = express();
@@ -12,12 +11,29 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-// /todoforserver
-mongoose
-  .connect("mongodb://eeee:eeee@127.0.0.1:27017/todoforserver")
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log(err));
 
+// MongoDB Connection
+mongoose
+  .connect("mongodb://eeee:eeee@127.0.0.1:27017/todoforserver?authSource=admin", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB!");
+    mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.error("Connection error:", err);
+  });
+
+
+// Log MongoDB connection errors
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+// Routes
 app.use("/api", routes);
 
+// Start Server
 app.listen(PORT, () => console.log(`Listening at ${PORT}...`));
