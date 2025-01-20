0fbc6bdc8327e47bfd5eb27de62bc8554581c29d
Naresh <banvro07@gmail.com>
2025-01-20 18:43:59 +0530

    server configration

--- backend/Server.js
+++ backend/Server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const routes = require("./routes/ToDoRoutes");
+
 const cors = require("cors");
 
 const app = express();
@@ -14,23 +15,9 @@ app.use(cors());
 
 // MongoDB Connection
 mongoose
-  .connect("mongodb://eeee:eeee@195.35.20.80:27017/todoforserver?authSource=admin", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB!");
-    mongoose.connection.close();
-  })
-  .catch((err) => {
-    console.error("Connection error:", err);
-  });
-
-
-// Log MongoDB connection errors
-mongoose.connection.on("error", (err) => {
-  console.error("MongoDB connection error:", err);
-});
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log("Error connecting to MongoDB:", err));
 
 // Routes
 app.use("/api", routes);
